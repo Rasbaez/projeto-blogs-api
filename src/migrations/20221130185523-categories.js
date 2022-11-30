b0cc1a1adf7bf80c360907e96500d754ee6fd3d9
Roberto Baez <rbaezsp@gmail.com>
2022-11-30 17:41:25 -0300

    feat: adicionando tabelas via migrations

--- src/migrations/20221130185523-categories.js
+++ src/migrations/20221130185523-categories.js
@@ -1,21 +1,18 @@
-'use strict';
-
 module.exports = {
-  up: async (queryInterface, Datatypes) => {
+  up: async (queryInterface, Sequelize) => {
 
-    await queryInterface.createTable('Categories', {
+    await queryInterface.createTable('categories', {
        id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Datatypes.STRING,
+        type: Sequelize.INTEGER,
        },
       name: {
         allowNull: false,
-        type:Datatypes.STRING,
+        type: Sequelize.STRING,
       },
       });
-
   },
 
   down: async (queryInterface, _Sequelize) => {
---
+++ src/migrations/20221130191819-blog_posts.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts',
+     { 
+      id: {
+        primaryKey: true,
+        allowNull:false,
+        autoIncrement:true,
+        type:Sequelize.INTEGER,
+      },
+      title:{
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type:Sequelize.INTEGER,
+     
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+     }
+     );
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+   await queryInterface.dropTable('BlogPosts');
+  }
+};
